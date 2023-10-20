7820a69cd6e573441a881e04ea196651ea558f40
AMANverma9118 <amanverma9118359330@gmail.com>
2023-10-20 20:15:52 +0530

    making first and second page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var last_name = document.getElementById("Lastname").value;
 var number = document.getElementById("number").value;
 var email = document.getElementById("email").value;
 var state = document.getElementById("State").value;
-
+const count=0;
 //Storing in localstorage
 
 var name = localStorage.setItem("Name" , first_name+last_name);
@@ -26,5 +26,28 @@ c=email;
 d=state;
 if(a.length==0 || b.length==0 || c==0 || d==0){
     alert("Fill all the information");
+    count++;
 }
+
+if(count>0)
+{
+    text();
 }
+
+
+
+const submit = document.querySelector('.btn1');
+const instruction = document.querySelector('.instruction');
+instruction.classList.add('active');
+
+}
+function exit(){
+    const submit = document.querySelector('.btn1');
+    const instruction = document.querySelector('.instruction');
+    const exit = document.querySelector('.info-btn_exit-btn');
+    window.location.href="index.html"
+    
+}
+
+
+
